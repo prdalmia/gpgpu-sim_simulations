f0ca40e4c3338914d6641cb5907d2268850a8c69
prdalmia <pdalmia@wisc.edu>
2020-11-06 22:20:17 +0530

    message

--- benchmarks/src/cuda/pannotia/pagerank/kernel.cu
+++ benchmarks/src/cuda/pannotia/pagerank/kernel.cu
@@ -371,25 +371,24 @@ pagerank1(int * row, int * col, int * data, float * page_rank1,
       );
     }
 
-    asm volatile
-    (
-      ".reg .s32 aa1;\n\t"                    // Register for nid loaded from col
-      ".reg .s64 aa9;\n\t"                    // Register for casted nid value
-      ".reg .u64 aa17;\n\t"                   // Register for multiplied nid value as address
-      ".reg .u64 aa25;\n\t"                   // Register for final address to load from page_rank2
-      ".reg .f32 aa33;\n\t"                   // (Unused) Register for atomicAdd
-    );
-      for (; edge < end; ++edge) {
+    for (; edge < end; ++edge) {
       int * const col_base_addr = &col[edge];
 
       asm volatile
       (
+        "{\n\t"
+        ".reg .s32 aa1;\n\t"                    // Register for nid loaded from col
+        ".reg .s64 aa9;\n\t"                    // Register for casted nid value
+        ".reg .u64 aa17;\n\t"                   // Register for multiplied nid value as address
+        ".reg .u64 aa25;\n\t"                   // Register for final address to load from page_rank2
+        ".reg .f32 aa33;\n\t"                   // (Unused) Register for atomicAdd
+
         "ld.s32 aa1, [%0+0];\n\t"               // Load nid
         //"cvt.s64.s32 a9, a1;\n\t"              // Cast nid value
         "mul.wide.s32 aa17, aa1, 4;\n\t"         // Multiply nid for page_rank2 address calculation
         "add.u64 aa25, m42, aa17;\n\t"           // Final address calculation for page_rank2
         "atom.add.f32 aa33, [aa25], m41;\n\t"    // atomicAdd for addr0
-
+        "}"
         :                                      // Outputs
         : "l"(col_base_addr)                   // Inputs
       );
