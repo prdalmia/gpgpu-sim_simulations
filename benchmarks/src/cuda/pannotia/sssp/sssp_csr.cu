cc0f93030863493443ade2ac815ed6892fbbd64e
prdalmia <pdalmia@wisc.edu>
2020-11-10 21:49:48 +0530

    message

--- benchmarks/src/cuda/pannotia/sssp/sssp_csr.cu
+++ benchmarks/src/cuda/pannotia/sssp/sssp_csr.cu
@@ -102,7 +102,7 @@ int main(int argc, char **argv)
         csr = parseMetis_transpose(tmpchar, &num_nodes, &num_edges, directed);
     } else if (file_format == 0) {
         csr = parseCOO_transpose(tmpchar, &num_nodes, &num_edges, directed);
-    } else if (file_format == 2)             // Matrix market
+    } else if (file_format == 2) {            // Matrix market
         csr = parseMM(tmpchar, &num_nodes, &num_edges, directed, 0);
     } else {
         printf("reserve for future");
